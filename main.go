077078598df180eaa18e1c60a57779bd370b908f
masx200 <34191203+masx200@users.noreply.github.com>
2024-03-20 21:39:35 +0800

    添加注释

--- main.go
+++ main.go
@@ -196,9 +196,17 @@ func (c *customRoundTripperLoadBalancer) RoundTrip(req *http.Request) (*http.Res
 	return rs, err
 }
 
+// randomShuffle 函数用于对指定的切片进行随机打乱。
+// [T any] 表示该函数适用于任意类型的切片。
+// arr []T 是输入的切片，函数会对其原地打乱顺序。
+// 返回值 []T 是打乱后的切片。
 func randomShuffle[T any](arr []T) []T {
+	// 使用当前时间的纳秒级种子初始化随机数生成器，以确保每次运行结果都不同。
 	rand.Seed(time.Now().UnixNano())
+	// 使用 rand.Shuffle 函数来随机打乱切片的顺序。
+	// 这个函数会传入切片的长度以及一个交换元素的函数。
 	rand.Shuffle(len(arr), func(i, j int) {
+		// 交换函数通过交换 arr[i] 和 arr[j] 来打乱顺序。
 		arr[i], arr[j] = arr[j], arr[i]
 	})
 	return arr

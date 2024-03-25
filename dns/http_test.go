a63893eb2c411b97e7d98ff30625a301dd2ae7c9
masx200 <34191203+masx200@users.noreply.github.com>
2024-03-25 16:18:50 +0800

    Update http_test.go

--- dns/http_test.go
+++ dns/http_test.go
@@ -10,37 +10,54 @@ import (
 	"testing"
 )
 
+// TestHttpViaIP 通过指定的IP地址对给定的URL进行HTTP请求测试。
+//
+// 参数:
+//
+//	t *testing.T - 测试框架提供的测试上下文，用于报告测试失败和日志记录。
+//
+// 无返回值。
 func TestHttpViaIP(t *testing.T) {
-	ip := "2606:2800:220:1:248:1893:25c8:1946" // 替换为你要访问的IP地址
-	url := "http://www.example.com/"           // 替换为你要访问的URL
+	ip := "2606:2800:220:1:248:1893:25c8:1946" // 要访问的目标IP地址
+	url := "http://www.example.com/"           // 要访问的目标URL
 
-	resp, err := fetchWithIP(ip, url)
+	// 使用指定的IP地址发起HTTP GET请求
+	resp, err := FetchWithIP(ip, url)
 	if err != nil {
 		fmt.Println("Error:", err)
 		t.Errorf(err.Error())
 		return
 	}
 
+	// 确保响应体在函数返回前被关闭
 	defer resp.Body.Close()
+	// 读取并打印响应体内容
 	body, _ := io.ReadAll(resp.Body)
 	fmt.Println("Response:", string(body))
 }
+
+// TestHttpsViaIP 通过指定的IP地址测试HTTPS连接。
+// 参数:
+//
+//	t *testing.T - 测试框架提供的测试上下文，用于报告测试失败和日志记录。
+//
+// 无返回值。
 func TestHttpsViaIP(t *testing.T) {
-	ip := "2606:2800:220:1:248:1893:25c8:1946" // 替换为你要访问的IP地址
-	url := "https://www.example.com/"          // 替换为你要访问的URL
+	ip := "2606:2800:220:1:248:1893:25c8:1946" // 要访问的IP地址
+	url := "https://www.example.com/"          // 要访问的URL
 
-	resp, err := fetchWithIP(ip, url)
+	resp, err := FetchWithIP(ip, url)
 	if err != nil {
 		fmt.Println("Error:", err)
 		t.Errorf(err.Error())
 		return
 	}
 
-	defer resp.Body.Close()
+	defer resp.Body.Close() // 确保在函数返回前关闭响应体
 	body, _ := io.ReadAll(resp.Body)
-	fmt.Println("Response:", string(body))
+	fmt.Println("Response:", string(body)) // 打印响应内容
 }
-func fetchWithIP(ip, url string) (*http.Response, error) {
+func FetchWithIP(ip, url string) (*http.Response, error) {
 	dialer := &net.Dialer{
 		// Timeout:   30 * time.Second,
 		// KeepAlive: 30 * time.Second,

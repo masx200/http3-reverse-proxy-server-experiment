4e092ae2f3c7101d3eb0602a5b0c70a1aacafe81
masx200 <34191203+masx200@users.noreply.github.com>
2024-03-26 13:51:03 +0800

    Update load_balance.go

--- load_balance/load_balance.go
+++ load_balance/load_balance.go
@@ -75,7 +75,7 @@ type MapInterface[T any, Y any] interface {
 
 // Pair 是一个泛型结构体，用于存储一对任意类型的值。
 // T和Y是泛型参数，代表First和Second可以是任何类型。
-type Pair[T any, Y any] struct {
+type PairImplement[T any, Y any] struct {
 	First  T // First是结构体中的第一个元素。
 	Second Y // Second是结构体中的第二个元素。
 }
@@ -85,4 +85,24 @@ type PairInterface[T any, Y any] interface {
 	GetSecond() Y
 
 	SetSecond(Y)
+}
+
+// 实现 GetFirst 方法
+func (p *PairImplement[T, Y]) GetFirst() T {
+	return p.First
+}
+
+// 实现 SetFirst 方法
+func (p *PairImplement[T, Y]) SetFirst(first T) {
+	p.First = first
+}
+
+// 实现 GetSecond 方法
+func (p *PairImplement[T, Y]) GetSecond() Y {
+	return p.Second
+}
+
+// 实现 SetSecond 方法
+func (p *PairImplement[T, Y]) SetSecond(second Y) {
+	p.Second = second
 }

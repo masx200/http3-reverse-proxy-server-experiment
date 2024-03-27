6b3180c30a5e0f54b18da3b1d80e95ddbd2963d9
masx200 <34191203+masx200@users.noreply.github.com>
2024-03-27 17:13:46 +0800

    Update doq_test.go

--- dns/doq_test.go
+++ dns/doq_test.go
@@ -1,20 +1,20 @@
 package dns
 
 import (
-	"context"
+	// "context"
 	"fmt"
 	"log"
 	"testing"
 
 	"github.com/miekg/dns"
-	doq "github.com/tantalor93/doq-go/doq"
+	// doq "github.com/tantalor93/doq-go/doq"
 )
 
 func TestDOQ(t *testing.T) {
 	// 创建一个新的 DoQ 客户端
 	x := "family.adguard-dns.com:853"
 	doqServer := "quic://" + x
-	client := doq.NewClient(x, doq.Options{})
+	// client := doq.NewClient(x, doq.Options{})
 	// if err != nil {
 	// 	panic(err)
 	// }
@@ -24,7 +24,7 @@ func TestDOQ(t *testing.T) {
 	// 查询 A 记录
 	qA := dns.Msg{}
 	qA.SetQuestion(domain+".", dns.TypeA)
-	respA, err := client.Send(context.Background(), &qA)
+	respA, err := DOQClient(&qA, doqServer) //client.Send(context.Background(), &qA)
 	if err != nil {
 		fmt.Println("Error querying A record:", err)
 		t.Fatal(err)
@@ -45,7 +45,7 @@ func TestDOQ(t *testing.T) {
 	// 查询 AAAA 记录
 	qAAAA := dns.Msg{}
 	qAAAA.SetQuestion(domain+".", dns.TypeAAAA)
-	respAAAA, err := client.Send(context.Background(), &qAAAA)
+	respAAAA, err := DOQClient(&qAAAA, doqServer) //client.Send(context.Background(), &qAAAA)
 	if err != nil {
 		fmt.Println("Error querying AAAA record:", err)
 		t.Fatal(err)
@@ -67,7 +67,7 @@ func TestDOQ(t *testing.T) {
 	// 注意：这里假设服务器支持并返回 HTTPS 记录
 	qHTTPS := dns.Msg{}
 	qHTTPS.SetQuestion(domain+".", dns.TypeHTTPS)
-	respHTTPS, err := client.Send(context.Background(), &qHTTPS)
+	respHTTPS, err := DOQClient(&qHTTPS, doqServer) //client.Send(context.Background(), &qHTTPS)
 	if err != nil {
 		fmt.Println("Error querying HTTPS record:", err)
 		t.Fatal(err)

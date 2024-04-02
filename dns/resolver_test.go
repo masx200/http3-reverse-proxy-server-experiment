ad0ed5fcad63690abab5e05baff110aca73be63d
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-02 15:37:18 +0800

    Update resolver_test.go

--- dns/resolver_test.go
+++ dns/resolver_test.go
@@ -22,3 +22,18 @@ func TestResolver(t *testing.T) {
 		fmt.Println(x, result)
 	}
 }
+func TestResolver2(t *testing.T) {
+	x := "nextjs-doh-reverse-proxy.onrender.com"
+	results, err := DnsResolver(func(m *dns.Msg) (r *dns.Msg, err error) {
+		return DohClient(m, "https://cloudflare-dns.com/dns-query")
+	}, x)
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	for _, result := range results {
+		fmt.Println(x, result)
+	}
+}

ad1f4c7afb7235ebd673165ca751c5d9c8f8b2be
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-02 16:11:39 +0800

    Update resolver_test.go

--- dns/resolver_test.go
+++ dns/resolver_test.go
@@ -69,3 +69,20 @@ func TestResolver4(t *testing.T) {
 		fmt.Println(x, result)
 	}
 }
+func TestResolverMultipleServers(t *testing.T) {
+	x := "hello-word-worker-cloudflare.masx200.workers.dev"
+	results, err := DnsResolverMultipleServers([]func(m *dns.Msg) (r *dns.Msg, err error){func(m *dns.Msg) (r *dns.Msg, err error) {
+		return DohClient(m, "https://cloudflare-dns.com/dns-query")
+	}, func(m *dns.Msg) (r *dns.Msg, err error) {
+		return DohClient(m, "https://dns.alidns.com/dns-query")
+	}}, x)
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	for _, result := range results {
+		fmt.Println(x, result)
+	}
+}

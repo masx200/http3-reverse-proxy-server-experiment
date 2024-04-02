5bca1e33a6c05f6664542c93b1a9e541fec16f1f
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-02 15:29:56 +0800

    Create resolver_test.go

---
+++ dns/resolver_test.go
@@ -0,0 +1,24 @@
+package dns
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/miekg/dns"
+)
+
+func TestResolver(t *testing.T) {
+	x := "hello-word-worker-cloudflare.masx200.workers.dev"
+	results, err := DnsResolver(func(m *dns.Msg) (r *dns.Msg, err error) {
+		return DohClient(m, "https://cloudflare-dns.com/dns-query")
+	}, x)
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	for _, result := range results {
+		fmt.Println(x, result)
+	}
+}

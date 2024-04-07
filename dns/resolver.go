c4900a6cfb28a57e6345b077400d4f77431a4be3
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-07 22:00:25 +0800

    Update resolver.go

--- dns/resolver.go
+++ dns/resolver.go
@@ -290,3 +290,69 @@ func ArrayMap[T any, U any](arr []T, fn func(T) U) []U {
 
 	return result
 }
+
+type MapImplementSynchronous[T comparable, Y any] struct {
+	data       generic.MapInterface[T, Y]
+	cacheMutex sync.Mutex
+}
+
+// Clear implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Clear() {
+	panic("unimplemented")
+}
+
+// Delete implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Delete(T) {
+	panic("unimplemented")
+}
+
+// Entries implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Entries() []generic.PairInterface[T, Y] {
+	panic("unimplemented")
+}
+
+// ForEach implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) ForEach(func(Y, T, generic.MapInterface[T, Y])) {
+	panic("unimplemented")
+}
+
+// Get implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Get(T) (Y, bool) {
+	panic("unimplemented")
+}
+
+// Has implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Has(T) bool {
+	panic("unimplemented")
+}
+
+// Iterator implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Iterator() generic.Iterator[generic.PairInterface[T, Y]] {
+	panic("unimplemented")
+}
+
+// Keys implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Keys() []T {
+	panic("unimplemented")
+}
+
+// Set implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Set(T, Y) {
+	panic("unimplemented")
+}
+
+// Size implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Size() int64 {
+	panic("unimplemented")
+}
+
+// Values implements generic.MapInterface.
+func (m *MapImplementSynchronous[T, Y]) Values() []Y {
+	panic("unimplemented")
+}
+
+func NewMapImplementSynchronous[T comparable, Y any](entries ...generic.PairInterface[T, Y]) generic.MapInterface[T, Y] {
+	return &MapImplementSynchronous[T, Y]{
+		data: generic.NewMapImplement[T, Y](entries...),
+	}
+}

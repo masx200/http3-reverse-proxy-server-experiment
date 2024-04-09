17befa5d9cece538d6446de721ed61a5f5b01fbd
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-09 12:38:30 +0800

    Create doh3_debugger.go

---
+++ doh3_debugger/doh3_debugger.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	h3_experiment "github.com/masx200/http3-reverse-proxy-server-experiment/h3"
+	"github.com/miekg/dns"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		fmt.Println("usage:", os.Args[0], "domain dnstype dohurl")
+		return
+	}
+
+	var domain = os.Args[1]
+	var dnstype = os.Args[2]
+	var dohurl = os.Args[3]
+	fmt.Println("domain:", domain, "dnstype:", dnstype, "dohurl:", dohurl)
+
+	for _, d := range strings.Split(domain, ",") {
+		for _, t := range strings.Split(dnstype, ",") {
+			var msg = &dns.Msg{}
+			msg.SetQuestion(d+".", dns.StringToType[t])
+			fmt.Println(msg.String())
+
+			res, err := h3_experiment.DoHTTP3Client(msg, dohurl)
+			if err != nil {
+				fmt.Println(err)
+				return
+
+			}
+			fmt.Println(res.String())
+		}
+	}
+
+}
